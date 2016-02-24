89f85d709346980b18c964ac0a70f91e95e0b891
robinvierich <robinvierich@gmail.com>
2016-02-23 23:32:37 -0500

    Basic rendering

--- engine_project/main.cpp
+++ engine_project/main.cpp
@@ -7,6 +7,9 @@
 #include <GL\glew.h>
 #include <GL\wglew.h>
 
+#include <OVR_CAPI.h>
+#include <OVR_CAPI_GL.h>
+
 #pragma comment(lib, "glew32.lib")
 
 #define DBOUT( s )            \
@@ -89,28 +92,15 @@ void setupScene() {
 }
 
 
-int width, height = 0;
+int windowWidth, windowHeight = 0;
 HDC hdc = NULL; // device context handle
 HGLRC glcxt = NULL; // OpenGL rendering context handle
+GLuint fbo = 0;
 
-void renderScene() {
-    glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // clear the current color buffer (default framebuffer), depth buffer, and stencil buffer
-
-    glUseProgram(shaderProgram); // set our shader program to be the current shaders
-
-    glBindVertexArray(vao); // set (vao) to be the current vertex attribute object
-
-    glDrawArrays(GL_TRIANGLES, 0, 3); 
-        // Draw (3) (GL_TRIANGLES). Get the position data from the currently bound vertex array (vao), and start reading at index (0)
-        //   What happens if we have normals as well??
-
-    SwapBuffers(hdc); // windows command
-}
 
 void resizeWindow(int w, int h) {
-    width = w;
-    height = h;
+    windowWidth = w;
+    windowHeight = h;
 }
 
 bool initOpenGL(HWND hWnd) {
@@ -157,9 +147,184 @@ bool initOpenGL(HWND hWnd) {
     glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]); // use GLEW to get the Major version of OpenGL we're using
     glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]); // use GLEW to get the Minor version of OpenGL we're using
 
-    DBOUT("Using OpenGL " << glVersion[0] << "." << glVersion[1] << std::endl);
+    DBOUT("Using OpenGL " << glVersion[0] << "." << glVersion[1] << std::endl);   
+
+    glGenFramebuffers(1, &fbo);
 
     return true;
+}
+
+// Create VR Structures.
+ovrSession session;
+ovrEyeRenderDesc eyeRenderDesc[2];
+ovrVector3f hmdToEyeViewOffset[2];
+ovrLayerEyeFov layer;
+ovrHmdDesc hmdDesc;
+
+ovrTexture textures[2];
+
+ovrSwapTextureSet * pTextureSet = 0;
+
+void initOcr() {
+    ovrResult result = ovr_Initialize(nullptr);
+
+    // enable SRGB textures in the FrameBuffer. Occulus expects srgb textures when rendering to eyes.
+    glEnable(GL_FRAMEBUFFER_SRGB);
+
+    if (OVR_FAILURE(result)) {
+        return;
+    }
+
+    ovrGraphicsLuid luid;
+    result = ovr_Create(&session, &luid); // fails if no rift is plugged in.
+
+    if (OVR_FAILURE(result)) {
+        ovr_Shutdown();
+        return;
+    }
+
+    
+    // Set up VR Structures.
+
+    hmdDesc = ovr_GetHmdDesc(session);
+
+    // initialize eye descriptions
+    eyeRenderDesc[0] = ovr_GetRenderDesc(session, ovrEye_Left, hmdDesc.DefaultEyeFov[0]);
+    eyeRenderDesc[1] = ovr_GetRenderDesc(session, ovrEye_Right, hmdDesc.DefaultEyeFov[0]);
+
+    hmdToEyeViewOffset[0] = eyeRenderDesc[0].HmdToEyeViewOffset;
+    hmdToEyeViewOffset[1] = eyeRenderDesc[1].HmdToEyeViewOffset;
+
+    // get recommended texture sizes for each eye
+    ovrSizei resolution = hmdDesc.Resolution;
+
+    ovrSizei recommenedTex0Size = ovr_GetFovTextureSize(
+        session, 
+        ovrEye_Left,
+        hmdDesc.DefaultEyeFov[0],
+        1.0f);
+
+    ovrSizei recommenedTex1Size = ovr_GetFovTextureSize(
+        session, 
+        ovrEye_Right,
+        hmdDesc.DefaultEyeFov[1], 
+        1.0f);
+
+    // use recommended sizes for eye frame buffer
+    ovrSizei bufferSize;
+    bufferSize.w = recommenedTex0Size.w + recommenedTex1Size.w;
+    bufferSize.h = max(recommenedTex0Size.h, recommenedTex1Size.h);    
+
+    // create a (swap? because we ping-pong between left-right each frame?) texture set, and retrieve the GL textures from this set
+    result = ovr_CreateSwapTextureSetGL(session, GL_SRGB8_ALPHA8, bufferSize.w, bufferSize.h, &pTextureSet);
+
+    if (OVR_FAILURE(result)) {
+        ovr_Shutdown();
+        return;
+    }
+    
+    layer.Header.Type = ovrLayerType_EyeFov; // seems like a strange line.. since ovrLayerEyeFov could set this by default? since it's typed?
+    layer.Header.Flags = 0;
+    layer.ColorTexture[0] = pTextureSet; // framebuffer for left eye?
+    layer.ColorTexture[1] = pTextureSet; // framebuffer for right eye?
+    layer.Fov[0] = eyeRenderDesc[0].Fov;
+    layer.Fov[1] = eyeRenderDesc[1].Fov;
+
+    ovrRecti leftEyeViewport = {
+        {0, 0},                            // ovrVector2i position;
+        {bufferSize.w / 2, bufferSize.h} // ovrSizei size;
+    };
+    layer.Viewport[0] = leftEyeViewport;
+
+    ovrRecti rightEyeViewport = {
+        { bufferSize.w / 2, 0 },            // ovrVector2i position;
+        { bufferSize.w / 2, bufferSize.h } // ovrSizei size;
+    };
+
+    layer.Viewport[1] = rightEyeViewport;
+}
+
+void renderTriangle() {
+
+    glUseProgram(shaderProgram); // set our shader program to be the current shaders
+
+    glBindVertexArray(vao); // set (vao) to be the current vertex attribute object
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo); // make this array buffer active (global variable in opengl state maching - ex. array_buffer = vbo
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW); // update values in buffer
+
+                                                                              // set up our list of drawBuffers (buffers that GL will draw to?)
+    GLenum drawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+    glDrawBuffers(1, drawBuffers); // there is 1 draw buffer, contained within drawBuffers
+
+    // Draw (3) (GL_TRIANGLES). Get the position data from the currently bound vertex array (vao), and start reading at index (0)
+    //   What happens if we have normals as well??
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+void renderScene() {
+
+
+    
+    // Query the HMD for the current tracking state.
+    double displayMidpointSeconds = ovr_GetPredictedDisplayTime(session, 0);
+    ovrTrackingState hmdTrackingState = ovr_GetTrackingState(session, displayMidpointSeconds, ovrFalse);
+
+    // calculate the eye poses from the current headpose and hmdToEyeViewOffset
+    ovr_CalcEyePoses(hmdTrackingState.HeadPose.ThePose, hmdToEyeViewOffset, layer.RenderPose);
+
+    if (hmdTrackingState.StatusFlags & ovrStatus_PositionConnected) {
+        ovrPoseStatef pose = hmdTrackingState.HeadPose;
+        // debug code - update one vertex of the triangle to match normalized ovr position
+        points[0] = pose.ThePose.Position.x;
+        points[1] = pose.ThePose.Position.y;
+        points[2] = pose.ThePose.Position.z;
+    }
+
+    // Increment to use next texture, just before writing
+    pTextureSet->CurrentIndex = (pTextureSet->CurrentIndex + 1) % pTextureSet->TextureCount;
+
+    // grab left eye GL texture from the textureset
+    ovrGLTexture* tex = (ovrGLTexture*)&pTextureSet->Textures[pTextureSet->CurrentIndex];
+    GLuint texId = tex->OGL.TexId;
+
+    double eyeViewportX = layer.Viewport[pTextureSet->CurrentIndex].Pos.x;
+    double eyeViewportY = layer.Viewport[pTextureSet->CurrentIndex].Pos.y;
+    double eyeViewportWidth = layer.Viewport[pTextureSet->CurrentIndex].Size.w;
+    double eyeViewportHeight = layer.Viewport[pTextureSet->CurrentIndex].Size.h;
+
+    // bind a texture to write to
+    glBindTexture(GL_TEXTURE_2D, texId);
+    // clear the texture (image of 0s)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB8_ALPHA8, eyeViewportWidth, eyeViewportHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+    // filter image
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // bind a custom framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    // set our texture to our framebuffer's 'colour attachment'? Attach a 2d texture to the fbo
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texId, 0);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // clear the current color buffer (default framebuffer), depth buffer, and stencil buffer
+
+    glViewport(eyeViewportX, eyeViewportY, eyeViewportWidth, eyeViewportHeight);
+
+    renderTriangle();
+
+    ovrLayerHeader* layers = &layer.Header;
+    ovrResult result = ovr_SubmitFrame(session, 0, nullptr, &layers, 1);
+
+    glBindFramebuffer(GL_FRAMEBUFFER_EXT, 0);
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // clear the current color buffer (default framebuffer), depth buffer, and stencil buffer
+
+    glViewport(0, 0, windowWidth, windowHeight);
+    renderTriangle();
+
+    SwapBuffers(hdc); // windows command
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -232,7 +397,10 @@ int WINAPI WinMain(HINSTANCE hInstance,
         return 1;
     }
 
+    
+
     initOpenGL(hWnd);
+    initOcr();
     setupScene();
 
     ShowWindow(hWnd, nCmdShow);
@@ -243,6 +411,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     MSG msg;
     bool running = true; // keep track of our own 'running' var
+
     while (running) { 
         if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) { // if there is a message, and handle it (why don't we need to consume it?! -- i.e. GetMessage)
             if (msg.message == WM_QUIT) {
@@ -255,6 +424,10 @@ int WINAPI WinMain(HINSTANCE hInstance,
             renderScene();
         }
     }
+
+    // destroy occulus session
+    ovr_Destroy(session);
+    ovr_Shutdown();
 
     return (int)msg.wParam;
 }
