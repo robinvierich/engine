869367b96f3c9421f2a6e484d323681bbc942dbf
Robin <robinvierich@gmail.com>
2017-11-05 19:53:03 -0500

    Started working on a GLRenderer class and surrounding structures (will reorganize when the design is more settled)

---
+++ main/GLRenderer.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "Math/Math.h"
+#include "Core/String.h"
+#include "Frame.h"
+
+#include <GL\glew.h>
+
+
+enum ShaderType
+{
+    Vertex,
+    Fragment
+};
+
+class Shader
+{
+    public:
+        Shader(const String _source, const ShaderType _type)
+            : m_glId(0)
+            , m_source(_source)
+            , m_type(_type)
+        {
+        }
+
+        GLuint m_glId;
+        const String m_source;
+        const ShaderType m_type;
+};
+
+class RenderableThing
+{
+    public:
+        Shader m_vertexShader;
+        Shader m_fragmentShader;
+
+
+};
+
+class GLRenderer
+{
+    private:
+        Matrix4 m_projectionMatrix;
+
+    public:
+
+        GLRenderer();
+
+        void init();
+        void shutdown();
+        void render(Frame& frame);
+};
